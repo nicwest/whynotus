b57bf8f69411c53740a772f703312e69cd32c697
Nic West <nicwest@mailbox.org>
2024-07-29 19:59:50 +0100

    fix tests

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import {App} from './App';
 
-test('renders learn react link', () => {
+test('renders the website name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/For potential employees/i);
   expect(linkElement).toBeInTheDocument();
 });

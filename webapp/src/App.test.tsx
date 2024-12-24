b29ef9f1504ea755f547df88e74e64827825b45c
Nic West <nicwest@mailbox.org>
2024-12-24 12:31:59 +0000

    fix tests

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import {App} from './App';
+import {
+  MemoryRouter,
+} from "react-router-dom";
 
 test('renders the website name', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const linkElement = screen.getByText(/For potential employees/i);
   expect(linkElement).toBeInTheDocument();
 });
